62afc7826f6ce58613a5928f43875a2b4b3b7760
Etienne Sibeaux <esibeaux.pro@outlook.fr>
2023-02-04 16:13:54 +0100

    on peut pousser ces fdp

--- Assets/Tech/Scripts/Character/CS_Character.cs
+++ Assets/Tech/Scripts/Character/CS_Character.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CS_Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer _sr;
+    [SerializeField] private Sprite _groundedSprite, _hopSprite;
+    private bool _canHop = true;
+
+    private Coroutine _currentCor;
+
+    private void Start()
     {
-        
+        _currentCor = StartCoroutine(HopCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCanHop(bool canHop)
     {
-        
+        _canHop = canHop;
+
+        if (_canHop)
+        {
+            _currentCor = StartCoroutine(HopCoroutine());
+        }
+        else
+        {
+            StopCoroutine(_currentCor);
+            _currentCor = null;
+        }
+    }
+
+    private IEnumerator HopCoroutine()
+    {
+        while (true)
+        {
+            float delay = Random.Range(0.9f, 3.3f);
+            yield return new WaitForSeconds(delay);
+            ChangeSprite(true);
+            yield return new WaitForSeconds(0.1f);
+            ChangeSprite(false);
+        }
+    }
+
+    private void ChangeSprite(bool hop)
+    {
+        _sr.sprite = hop ? _hopSprite : _groundedSprite;
     }
 }
--- Assets/Tech/Scripts/Movement/CS_Movement.cs
+++ Assets/Tech/Scripts/Movement/CS_Movement.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CS_Movement : MonoBehaviour
 {
-    [SerializeField] private float _walkSpeed = 10f, _dashSpeed = 30f, _dashDelay = 1, _throwDelay = 1;
+    [SerializeField] private float _walkSpeed = 10f, _dashSpeed = 30f, _beingPushedSpeed, _dashDelay = 1, _throwDelay = 1, _beingPushedDelay = 1;
     [SerializeField] private CharacterController _cc;
     [SerializeField] private CS_Indicators _indicators;
+    [SerializeField] private Sprite _idleSprite, _dashSprite, _hurtSprite;
 
-    private Vector2 _lastDirection = new Vector2(-1, 0);
+    private Vector2 _lastDirection = new Vector2(0, -1);
+    private Vector2 _beingPushedDir;
+    private Vector2 _dashDir;
 
     private Vector2 _inputDirection;
     private float yDir;
 
     private bool _hasToDash, _isDashing;
-    private bool _canDash => (!_isDashing && !_hasToDash && !_isThrowing);
+    private bool _canDash => (!_isDashing && !_hasToDash && !_isThrowing && !_isBeingPushed);
     public bool IsDashing => _isDashing;
 
     private bool _hasToThrow, _isThrowing;
-    private bool _canThrow => (!_isThrowing && !_hasToThrow && !_isDashing);
+    private bool _canThrow => (!_isThrowing && !_hasToThrow && !_isDashing && !_isBeingPushed);
     public bool IsThrowing => _isThrowing;
+
+    private bool _isBeingPushed;
+    public bool IsBeingPushed => _isBeingPushed;
+
+    private void Start()
+    {
+        UpdateLastDir(_lastDirection);
+    }
+
+    #region Register Inputs
 
     public void RegisterMove(Vector2 direction)
     {
@@ -31,12 +45,6 @@ public class CS_Movement : MonoBehaviour
 
         if (direction != Vector2.zero)
             UpdateLastDir(direction.normalized);
-    }
-
-    private void UpdateLastDir(Vector2 dir)
-    {
-        _lastDirection = dir;
-        _indicators.SetOrientation(Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg);
     }
 
     public void RegisterDash()
@@ -55,10 +63,15 @@ public class CS_Movement : MonoBehaviour
         }
     }
 
+    #endregion Register Inputs
+
+    #region Fixed Updates
+
     private void FixedUpdate()
     {
         CalculateGravity();
         Move();
+        BeingPushed();
         Dash();
         Throw();
     }
@@ -76,6 +89,9 @@ public class CS_Movement : MonoBehaviour
 
     private void Move()
     {
+        if (_isBeingPushed)
+            return;
+
         Vector2 actualDir = _inputDirection;
 
         if (_isDashing)
@@ -90,6 +106,17 @@ public class CS_Movement : MonoBehaviour
         _cc.Move(trueDirection);
     }
 
+    private void BeingPushed()
+    {
+        if (!_isBeingPushed)
+            return;
+
+        Debug.Log($"Being pushed {transform.name} {_beingPushedDir}");
+
+        Vector3 dirToBeingPushed = new Vector3(_beingPushedDir.x, 0, _beingPushedDir.y);
+        _cc.Move(dirToBeingPushed * _beingPushedSpeed * Time.deltaTime);
+    }
+
     private void Dash()
     {
         if (!_hasToDash && !_isDashing)
@@ -101,7 +128,10 @@ public class CS_Movement : MonoBehaviour
             StartCoroutine(DashCoroutine());
         }
         else if (!_hasToDash)
-            _cc.Move(_lastDirection * _dashSpeed * Time.deltaTime);
+        {
+            Vector3 dirToDash = new Vector3(_dashDir.x, 0, _dashDir.y);
+            _cc.Move(dirToDash * _dashSpeed * Time.deltaTime);
+        }
     }
 
     private void Throw()
@@ -112,6 +142,21 @@ public class CS_Movement : MonoBehaviour
         _hasToThrow = false;
         StartCoroutine(ThrowCoroutine());
     }
+
+    #endregion Fixed Updates
+
+    private void StopDashDirection()
+    {
+        _dashDir = Vector2.zero;
+    }
+
+    private void UpdateLastDir(Vector2 dir)
+    {
+        _lastDirection = dir;
+        _indicators.SetOrientation(Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg);
+    }
+
+    #region Coroutines
 
     private IEnumerator DashCoroutine()
     {
@@ -129,24 +174,58 @@ public class CS_Movement : MonoBehaviour
         SetIsThrowing(false);
     }
 
+    private IEnumerator BeingPushedCoroutine()
+    {
+        _isBeingPushed = true;
+        SetInBeingHurt(true);
+        yield return new WaitForSeconds(_beingPushedDelay);
+        _isBeingPushed = false;
+        _beingPushedDir = Vector2.zero;
+        SetInBeingHurt(false);
+    }
+
+    #endregion Coroutines
+
+    #region Setters
+
+    public void StartBeingPushed(Vector2 dir)
+    {
+        if (_isBeingPushed || _isDashing)
+            return;
+
+        _beingPushedDir = dir;
+        StartCoroutine(BeingPushedCoroutine());
+    }
+
     private void SetIsDashing(bool value)
     {
         _isDashing = value;
+        _dashDir = _lastDirection;
 
         if (_isDashing)
         {
             foreach (var item in GetComponentsInChildren<SpriteRenderer>())
             {
-                if (item.transform.name != "Shadow")
-                    item.color = Color.red;
+                if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
+                    item.sprite = _dashSprite;
+            }
+
+            foreach (var item in GetComponentsInChildren<CS_Character>())
+            {
+                item.SetCanHop(false);
             }
             return;
         }
 
         foreach (var item in GetComponentsInChildren<SpriteRenderer>())
         {
-            if (item.transform.name != "Shadow")
-                item.color = Color.white;
+            if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
+                item.sprite = _idleSprite;
+        }
+
+        foreach (var item in GetComponentsInChildren<CS_Character>())
+        {
+            item.SetCanHop(true);
         }
     }
 
@@ -158,7 +237,7 @@ public class CS_Movement : MonoBehaviour
         {
             foreach (var item in GetComponentsInChildren<SpriteRenderer>())
             {
-                if (item.transform.name != "Shadow")
+                if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
                     item.color = Color.green;
             }
             return;
@@ -166,8 +245,60 @@ public class CS_Movement : MonoBehaviour
 
         foreach (var item in GetComponentsInChildren<SpriteRenderer>())
         {
-            if (item.transform.name != "Shadow")
+            if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
                 item.color = Color.white;
         }
     }
+
+    private void SetInBeingHurt(bool value)
+    {
+        if (value)
+        {
+            foreach (var item in GetComponentsInChildren<SpriteRenderer>())
+            {
+                if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
+                    item.sprite = _hurtSprite;
+            }
+
+            foreach (var item in GetComponentsInChildren<CS_Character>())
+            {
+                item.SetCanHop(true);
+            }
+            return;
+        }
+
+        foreach (var item in GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (item.transform.name.Contains("Carotte") || item.transform.name.Contains("Patate"))
+                item.sprite = _idleSprite;
+        }
+
+        foreach (var item in GetComponentsInChildren<CS_Character>())
+        {
+            item.SetCanHop(true);
+        }
+    }
+
+    #endregion Setters
+
+    #region Other Players Actions
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (_dashDir == Vector2.zero || !_isDashing)
+            return;
+
+        if (hit.transform.TryGetComponent(out CS_Movement otherPlayer))
+        {
+            Push(otherPlayer);
+        }
+    }
+
+    private void Push(CS_Movement otherPlayer)
+    {
+        otherPlayer.StartBeingPushed(_dashDir);
+        StopDashDirection();
+    }
+
+    #endregion Other Players Actions
 }
